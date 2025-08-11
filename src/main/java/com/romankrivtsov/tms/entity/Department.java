f4f453abecc6db875f236e015760498cb030b89b
r0maniast <romankrivtsov7@gmail.com>
2025-08-12 00:21:12 +0300

    create Department.java

---
+++ src/main/java/com/romankrivtsov/tms/entity/Department.java
@@ -0,0 +1,45 @@
+package com.romankrivtsov.tms.entity;
+
+import jakarta.persistence.*;
+
+import java.util.List;
+
+@Entity
+@Table(name = "departments")
+public class Department {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "departments_id_seq")
+    @SequenceGenerator(name = "departments_id_seq", allocationSize = 1)
+    @Column(name = "id")
+    int id;
+
+    @Column(name = "title")
+    public String title;
+
+    @OneToMany(mappedBy = "department")
+    private List<Employee> employees;
+
+    public Department() {
+    }
+
+    public Department(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    @Override
+    public String toString() {
+        return "department{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                '}';
+    }
+}
