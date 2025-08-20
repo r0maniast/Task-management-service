b65a1b76ce56b6c1524cd1b344b7498c9aea6826
r0maniast <romankrivtsov7@gmail.com>
2025-08-20 09:14:42 +0300

    create Employee Request DTO

---
+++ src/main/java/com/romankrivtsov/tms/dto/request/employee/EmployeeRequest.java
@@ -0,0 +1,69 @@
+package com.romankrivtsov.tms.dto.request.employee;
+
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public class EmployeeRequest {
+    @NotBlank(groups = CreateValidate.class)
+    @Size(min = 1, max = 15, groups = CreateValidate.class)
+    private String name;
+
+    @Size(min = 1, max = 15, groups = CreateValidate.class)
+    private String patronymic;
+
+    @NotBlank(groups = CreateValidate.class)
+    @Size(min = 1, max = 20, groups = CreateValidate.class)
+    private String surname;
+
+    @NotBlank(groups = CreateValidate.class)
+    @Size(max = 30, groups = CreateValidate.class)
+    private String position;
+
+    @NotBlank(groups = CreateValidate.class)
+    @Min(value = 1, groups = CreateValidate.class)
+    private int departmentId;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPatronymic() {
+        return patronymic;
+    }
+
+    public void setPatronymic(String patronymic) {
+        this.patronymic = patronymic;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public int getDepartmentId() {
+        return departmentId;
+    }
+
+    public void setDepartmentId(int departmentId) {
+        this.departmentId = departmentId;
+    }
+
+    public interface CreateValidate{}
+    public interface UpdateValidate{}
+}
